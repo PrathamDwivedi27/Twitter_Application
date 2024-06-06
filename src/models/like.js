a415ccddc31a1cec4021f4246ba098ad9a8b8c09
PrathamDwivedi27 <pratham.gungun12345@gmail.com>
2024-06-06 18:38:17 +0530

    added basic like and user model

---
+++ src/models/like.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const likeSchema=new mongoose.Schema({
+    onModel:{                               //kis model pe base hai. Hum ya to tweet pe like kar sakte hai ya comment pe 
+        type:String,
+        required:true,
+        enum:['Tweet','Comment']
+    },
+    likeable:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'               //jaise peeche ref use kar rhe the na hashtag mein ki tweet se reference hai . waise hi jab jyade cheez pe reference dena ho to refpath. Hum jo bhi like karenge uska tweetid ya comment id store karenge.
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    }
+},{timestamps:true})
+
+
+const Like=mongoose.model('Like',likeSchema);
+
+export default Like;
